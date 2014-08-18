d79cc0ede51910d1717797ab62286a9fd2e45431
saftaclaudia <saftaclaudia27@yahoo.com>
2014-08-18 23:06:08 +0300

    change the width
    input adn select width

--- form.php
+++ form.php
@@ -12,20 +12,20 @@
 		<div class="clearfix">
 			<div class="left">
 				<div class="block">
-					<label for="full-name">Full Name:</label><p class="red">*</p>
+					<label for="full-name">Full Name:<p class="red">*</p></label>
 					<input id="full-name" name="full_name" type="text" placeholder="Your Name Here">	
 				</div>
 				<div class="block">
-					<label for="date-birth">Date of Birth:</label><p class="red">*</p>
+					<label for="date-birth">Date of Birth:<p class="red">*</p></label>
 					<input id="date-birth" name="date_birth" type="text">	
 				</div>
 				<div class="block">
-					<label for="username">Usernam:</label><p class="red">*</p>
+					<label for="username">Usernam:<p class="red">*</p></label>
 					<input id="username" name="user_name" type="text">	
 				</div>
 				<legend>Password</legend>
 				<div class="block">
-					<label for="password">Password:</label><p class="red">*</p>
+					<label for="password">Password:<p class="red">*</p></label>
 					<input id="password" name="user_passwors" type="password">	
 				</div>
 				<legend>Address</legend>
@@ -45,73 +45,79 @@
 			</div>
 			<div class="right">
 				<div class="block">
-					<label for="age">Age:</label><p class="red">*</p>
+					<label for="age">Age:<p class="red">*</p></label>
 					<input id="age" name="user_age" type="text">	
 				</div>
-				<div class="block">
-					<label for="date">Select Date:</label><p class="red">*</p>
-						<div class="width-min">
-							<select  id="year" name="year">
-								<option value="1" selected>---</option>
-								<option value="2">2013</option>
-								<option value="3">2014</option>
-							</select>
-							<select  id="month" name="month">
-								<option value="1" selected>January</option>
-								<option value="2">February</option>
-								<option value="3">March</option>
-								<option value="4">April</option>
-								<option value="5">May</option>
-								<option value="6">June</option>
-								<option value="7">July</option>
-								<option value="8">August</option>
-								<option value="9">September</option>
-								<option value="10">October</option>
-								<option value="11">November</option>
-								<option value="12">December</option>
-							</select>
-							<select id="day" name="day">
-								<option value="1" selected>1st</option>
-								<option value="2" >2nd</option>
-								<option value="3" >3</option>
-								<option value="4" >4</option>
-								<option value="5" >5</option>
-								<option value="6" >6</option>
-								<option value="7" >7</option>
-								<option value="8" >8</option>
-								<option value="9" >9</option>
-								<option value="10" >10</option>
-								<option value="11" >11</option>
-								<option value="12" >12</option>
-								<option value="13" >13</option>
-								<option value="14" >14</option>
-								<option value="15" >15</option>
-								<option value="16" >16</option>
-								<option value="17" >17</option>
-								<option value="18" >18</option>
-								<option value="19" >19</option>
-								<option value="20" >20</option>
-								<option value="21" >21</option>
-								<option value="22" >22</option>
-								<option value="23" >23</option>
-								<option value="24" >24</option>
-								<option value="25" >25</option>
-								<option value="26" >26</option>
-								<option value="27" >27</option>
-								<option value="28" >28</option>
-								<option value="29" >29</option>
-								<option value="30" >30</option>
-								<option value="31" >31</option>
-							</select>
+				<div class="data block">
+					<label for="date">Select Date:<p class="red">*</p></label>
+						<div class="clearfix">
+							<div class="width10">
+								<select  id="year" name="year">
+									<option value="1" selected>---</option>
+									<option value="2">2013</option>
+									<option value="3">2014</option>
+								</select>
+							</div>
+							<div class="width20">
+								<select  id="month" name="month">
+									<option value="1" selected>January</option>
+									<option value="2">February</option>
+									<option value="3">March</option>
+									<option value="4">April</option>
+									<option value="5">May</option>
+									<option value="6">June</option>
+									<option value="7">July</option>
+									<option value="8">August</option>
+									<option value="9">September</option>
+									<option value="10">October</option>
+									<option value="11">November</option>
+									<option value="12">December</option>
+								</select>
+							</div>
+							<div class="width10">
+								<select id="day" name="day">
+									<option value="1" selected>1st</option>
+									<option value="2" >2nd</option>
+									<option value="3" >3</option>
+									<option value="4" >4</option>
+									<option value="5" >5</option>
+									<option value="6" >6</option>
+									<option value="7" >7</option>
+									<option value="8" >8</option>
+									<option value="9" >9</option>
+									<option value="10" >10</option>
+									<option value="11" >11</option>
+									<option value="12" >12</option>
+									<option value="13" >13</option>
+									<option value="14" >14</option>
+									<option value="15" >15</option>
+									<option value="16" >16</option>
+									<option value="17" >17</option>
+									<option value="18" >18</option>
+									<option value="19" >19</option>
+									<option value="20" >20</option>
+									<option value="21" >21</option>
+									<option value="22" >22</option>
+									<option value="23" >23</option>
+									<option value="24" >24</option>
+									<option value="25" >25</option>
+									<option value="26" >26</option>
+									<option value="27" >27</option>
+									<option value="28" >28</option>
+									<option value="29" >29</option>
+									<option value="30" >30</option>
+									<option value="31" >31</option>
+								</select>
+							</div>
 						</div>	
 				</div>
-				<div class="block">
-					<label for="email-address">Email Address:</label><p class="red">*</p>
+				<div class="block clearboth">
+					<label for="email-address">Email Address:<p class="red">*</p></label>
 					<input type="text" id="email-address"  name="email_address">	
 				</div>
 
 				<div class="block margin-possition">
-					<label for="confirm-password">Confirm Password:</label><p class="red">*</p>
+					<label for="confirm-password">Confirm Password:<p class="red">*</p></label>
 					<input id="confirm-password" name="confirm_password" type="password">	
 				</div>
 				<div class="block">
@@ -119,14 +125,13 @@
 					<input id="city" name="user_city" type="text">	
 				</div>
 				<div class="block">
-					<label for="post-code">Post Code:</label><p class="red">*</p>
+					<label for="post-code">Post Code:<p class="red">*</p></label>
 					<input id="post-code" name="post_code" type="text">	
 				</div>
-
 			</div>
 		<div class="clearboth">
 			<legend>Company Information</legend>
-			<div class="clearfix">
+			<div class="clearfix width32">
 				<div class="block width-adjust">
 					<label for="company-name">Company Name:</label>
 					<input id="company-name" name="company_name" type="text">
